49cca1f81dc4352a8cf65eae1fcf90394cc83be9
Purdy <pu12dy@yahoo.com>
2020-11-14 13:00:28 -0800

    a

---
+++ AdjListGraph.cpp
@@ -0,0 +1,18 @@
+/*
+ * AdjListGraph.cpp
+ *
+ *  Created on: Nov 14, 2020
+ *      Author: pu12d
+ */
+
+#include "AdjListGraph.h"
+
+AdjListGraph::AdjListGraph() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AdjListGraph::~AdjListGraph() {
+	// TODO Auto-generated destructor stub
+}
+
--- AdjListGraph.h
+++ AdjListGraph.h
@@ -5,16 +5,22 @@
  * SECTION       : MW: 2:30PM
  * DUE DATE      : 11/16/2020
  *****************************************************************************/
-#ifndef ADJLIST_H_
-#define ADJLIST_H_
+#ifndef ADJLISTGRAPH_H_
+#define ADJLISTGRAPH_H_
 #include "header.h"
 
-class AdjList {
-public:
-	AdjList();
-	virtual ~AdjList();
+class AdjListGraph {
+private:
+	int numVert;
+	list<string>* adjList;
+	void DFShelper(string vert, bool visisted[]);
 
+public:
+	AdjListGraph(int numVert);
+	virtual ~AdjListGraph();
+	void addEdge(string vert, int weight);
+	void DFS(string vert);
 
 };
 
-#endif /* ADJLIST_H_ */
+#endif /* ADJLISTGRAPH_H_ */
